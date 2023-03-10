8c67c307cba38b99b97f849acb3447b1ec500a8b
Guilherme Lucas Teixeira Silva <glts.snf21@uea.edu.br>
2023-03-10 16:03:47 -0400

    Update conta.c

--- conta.c
+++ conta.c
@@ -1,22 +1,35 @@
-%{
+% option yylineno % option noyywrap %
+{
 #include <stdio.h>
-%}
+  void yyerror(char *s);
+  %
+}
 
-%%
+% %
 
-"int"         { printf("Tipo: INT\n"); }
-[a-zA-Z]+     { printf("Identificador: %s\n", yytext); }
-[0-9]+        { printf("Valor: %s\n", yytext); }
-"="           { printf("Operador de atribuição\n"); }
-"("           { printf("Abre parênteses\n"); }
-")"           { printf("Fecha parênteses\n"); }
-";"           { printf("Fim de linha\n"); }
-"+"           { printf("Operador de adição\n"); }
-[a-zA-Z]+[ ]*[+][ ]*[a-zA-Z]+ { printf("Expressão: %s\n", yytext); }
+    "int"
+{
+  printf("Tipo: INT\n");
+}
+[a - zA - Z] + { printf("Identificador: %s\n", yytext); }
+[0 - 9] + { printf("Valor: %s\n", yytext); }
+"=" { printf("Operador de atribuição\n"); }
+"(" { printf("Abre parênteses\n"); }
+")" { printf("Fecha parênteses\n"); }
+";" { printf("Fim de linha\n"); }
+"+" { printf("Operador de adição\n"); }
+[a - zA - Z] + [] *[+][] * [a - zA - Z] + { printf("Expressão: %s\n", yytext); }
+. { yyerror("Caractere inválido"); }
 
-%%
+% %
 
-int main(int argc, char* argv[]) {
+    void yyerror(char *s)
+{
+  printf("Erro: %s\n", s);
+}
+
+int main(int argc, char *argv[])
+{
   yyin = fopen("conta.txt", "r");
   yylex();
   fclose(yyin);
