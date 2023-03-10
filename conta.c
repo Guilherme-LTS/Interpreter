7514221e80bd642d8531696db1cf8bcb78481f4f
Guilherme Lucas Teixeira Silva <glts.snf21@uea.edu.br>
2023-03-10 16:01:08 -0400

    Update conta.c

--- conta.c
+++ conta.c
@@ -1,25 +1,22 @@
-% {
+%{
 #include <stdio.h>
-      % }
+%}
 
-    % %
+%%
 
-    "int"
-{
-  printf("Tipo: INT\n");
-}
-[a - zA - Z] + { printf("Identificador: %s\n", yytext); }
-[0 - 9] + { printf("Valor: %s\n", yytext); }
-"=" { printf("Operador de atribuição\n"); }
-"(" { printf("Abre parênteses\n"); }
-")" { printf("Fecha parênteses\n"); }
-";" { printf("Fim de linha\n"); }
-"+" { printf("Operador de adição\n"); }
+"int"         { printf("Tipo: INT\n"); }
+[a-zA-Z]+     { printf("Identificador: %s\n", yytext); }
+[0-9]+        { printf("Valor: %s\n", yytext); }
+"="           { printf("Operador de atribuição\n"); }
+"("           { printf("Abre parênteses\n"); }
+")"           { printf("Fecha parênteses\n"); }
+";"           { printf("Fim de linha\n"); }
+"+"           { printf("Operador de adição\n"); }
+[a-zA-Z]+[ ]*[+][ ]*[a-zA-Z]+ { printf("Expressão: %s\n", yytext); }
 
-% %
+%%
 
-    int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
   yyin = fopen("conta.txt", "r");
   yylex();
   fclose(yyin);
